4b85d9733270da6a3bceacbfa66b0e2a4822e676
Anton P <anton@antonp.com>
2022-01-31 20:23:51 +0100

    pretty code fix

--- BSC/BotToken.sol
+++ BSC/BotToken.sol
@@ -782,8 +782,8 @@ contract BotToken is Context, IERC20, Ownable {
         walletFee = 0; // 0%
         buybackFee = 1; // 1%
 		
-		_buyBackUpperLimit = 10 * 10**_decimals;
-		_buyBackMinLimit = 10 * 10**_decimals;
+        _buyBackUpperLimit = 10 * 10**_decimals;
+        _buyBackMinLimit = 10 * 10**_decimals;
 
         swapAndLiquifyEnabled = true;
         // Initialization. End
